ecb9ae087b5a9ecad4011790eeb8e5b269a2af8c
Graham Mc Allister <grahammcallister@gmail.com>
2018-09-08 13:09:35 +0200

    Latest work

--- server.js
+++ server.js
@@ -2,14 +2,13 @@ var express = require('express');
 var router = express.Router();
 var mustacheExpress = require('mustache-express');
 
-// define the home page route
 router.get('/', function (req, res) {
-    res.render('index', { title: 'The Software Pilgrim', datetime: ''});
-  })
+    res.render('index', { title: 'The Software Pilgrim', datetime: new Date()});
+})
   
-  router.get('/GreatDeveloper', function (req, res) {
-    res.send('Great Developer')
-  })
+router.get('/GreatDeveloper', function (req, res) {
+    res.render('greatdeveloper', { title: 'The Software Pilgrim - Great Developer'});
+})
 
 var app = express();
 app.use(express.static('public'));
@@ -17,9 +16,5 @@ app.engine('html', mustacheExpress());
 app.set('views', 'views');
 app.set('view engine', 'html');
 app.use('/', router);
-
-// app.get('/', (req, res) => {
-//     res.send('Hello World');
-// });
 
 app.listen(process.env.PORT || 8080);
