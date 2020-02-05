03af17b74b76fd38bc82edee822cbe956cd5e563
Graham Mc Allister <grahammcallister@gmail.com>
2020-02-05 16:57:10 +0200

    Add Azure app insights

--- server.js
+++ server.js
@@ -1,25 +1,34 @@
 var express = require('express');
 var router = express.Router();
 var mustacheExpress = require('mustache-express');
+var appInsights = require('@microsoft/applicationinsights-web');
+
+const insights = new appInsights.ApplicationInsights({ config: { instrumentationKey: process.env.APPINSIGHTS_INSTRUMENTATIONKEY}});
+insights.loadAppInsights();
 
 router.get('/', function (req, res) {
     res.render('index', { title: 'The Software Pilgrim', datetime: new Date()});
+    insights.trackPageView();
 })
   
 router.get('/GreatDeveloper', function (req, res) {
     res.render('greatdeveloper', { title: 'The Software Pilgrim - Great Developer'});
+    insights.trackPageView();
 })
 
 router.get('/PilgrimResources', function (req, res) {
     res.render('developerresources', { title: 'The Software Pilgrim - Great Developer Resources'});
+    insights.trackPageView();
 })
 
 router.get('/BookmarkThis', function (req, res) {
     res.render('bookmarkthis', { title: 'The Software Pilgrim - Bookmarks, links and lessons learned'});
+    insights.trackPageView();
 })
 
 router.get('/ThingsImThinkingAbout', function (req, res) {
     res.render('thingsimthinkingabout', { title: 'The Software Pilgrim - Things I am thinking about'});
+    insights.trackPageView();
 })
 
 var app = express();
